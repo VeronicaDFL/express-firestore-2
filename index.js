e1d074ecaac35db2cb559b7ca40b9792efd7482b
Veronica De Felice <verodefelic@gmail.com>
2023-04-04 16:09:30 -0400

    ADD endpoint and imported to tvLibrary

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import {addNewTv, getAllTv} from './src/tvLibrary.js';
 
 
 const PORT = 3030;
@@ -8,6 +9,13 @@ const PORT = 3030;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+//get all tvshows
+app.get("/tvshows",getAllTv);
+//add tvshows
+add.post("/tvShows", addNewTv);
+
 
 
 
